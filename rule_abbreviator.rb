d3d832c1cd9542c0e185b44b15466403c1c87fd6
Dion Chan <hwchan91@gmail.com>
2018-07-25 15:03:21 +0800

    rule abbreviator: recusively expand nested abbrev

--- rule_abbreviator.rb
+++ rule_abbreviator.rb
@@ -1,21 +1,29 @@
 module RuleAbbreviator
   def expand_rules(rules)
-    rules.map do |rule|
-      rule_patterns = rule[:pattern].is_a?(Array) ? rule[:pattern] : [ rule[:pattern] ]
-      rule_patterns.map! do |pattern|
+    rules.each do |rule|
+      rule[:pattern] = rule[:pattern].is_a?(Array) ? rule[:pattern] : [ rule[:pattern] ]
+      rule[:pattern].map! do |pattern|
         pattern = pattern.split(" ") if pattern.is_a?(String)
         expand_abbrev(pattern)
-        pattern
       end
-
-      rule[:pattern] = rule_patterns
-      rule
     end
   end
 
-  def expand_abbrev(pattern)
-    $pat_abbrev.each do |abbrev, expansion|
-      pattern.map! { |sym| sym == abbrev ? expansion : sym }
+  def expand_abbrev(arr)
+    arr.map do |sym|
+      if sym.is_a? Array
+        expand_abbrev(sym)
+      else
+        convert_abbrev(sym)
+      end
+    end
+  end
+
+  def convert_abbrev(sym)
+    if $pat_abbrev.keys.any? { |abb| abb == sym }
+      $pat_abbrev[sym]
+    else
+      sym
     end
   end
 
